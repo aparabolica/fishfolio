8fc195fb77649d36548f7b98fed8e902c4fb7c24
Miguel Peixe <miguel@cardume.art.br>
2017-02-23 14:14:48 -0300

    fix accent mark sorting and redesign "currently viewing" paragraph

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,3 +1,30 @@
+function removeAccentMark(str) {
+
+  var map = {
+    a : /[\xE0-\xE6]/g,
+    A : /[\xC0-\xC6]/g,
+    e : /[\xE8-\xEB]/g,
+    E : /[\xC8-\xCB]/g,
+    i : /[\xEC-\xEF]/g,
+    I : /[\xCC-\xCF]/g,
+    o : /[\xF2-\xF6]/g,
+    O : /[\xD2-\xD6]/g,
+    u : /[\xF9-\xFC]/g,
+    U : /[\xD9-\xDC]/g,
+    c : /\xE7/g,
+    C : /\xC7/g,
+    n : /\xF1/g,
+    N : /\xD1/g,
+  };
+
+  for(var l in map ) {
+    var exp = map[l];
+    str = str.replace(exp, l);
+  }
+
+  return str;
+}
+
 (function(undefined) {
 
   module.exports = function(app) {
@@ -93,7 +120,7 @@
                 return -total;
                 break;
               case 'name':
-                return $filter('translate')(project.name);
+                return removeAccentMark($filter('translate')(project.name));
                 break;
               case 'year':
                 return -project.launch_year;
--- src/js/directives.js
+++ src/js/directives.js
@@ -133,7 +133,6 @@
                 followPointer: true,
                 backgroundColor: scope.backgroundColor || null,
                 formatter: function() {
-                  console.log(this);
                   return '<span class="gh-tooltip"><span class="label"><b>Recent code activity on GitHub</b></span><br/><span class="date">' + moment(this.key).format('L') + '</span> - <span class="total">' + this.y + ' commits</span></span>';
                 },
                 borderRadius: 0,
